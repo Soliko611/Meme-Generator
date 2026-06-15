2c69831ce07794ffe32fc86130638486237b3820
S K <soli.kolet@email.com>
2026-06-15 22:10:33 +0300

    meme-gen

--- js/controllers/gallery.js
+++ js/controllers/gallery.js
@@ -23,17 +23,20 @@ function onImgSelect(imgId) {
   setImg(imgId)
   renderMeme()
 }
- function toggleMenu() {
-            document.body.classList.toggle('menu-open')
-        }
+function toggleMenu() {
+  document.body.classList.toggle("menu-open")
+}
 
-       function onShowGallery(){
-        const elGallery = document.querySelector('.gallery-section')
+function onShowGallery() {
+  const elGallery = document.querySelector(".gallery-section")
 
-        elGallery.scrollIntoView({ behavior: "smooth",})
-       }
-       function onShowAbout(){
-         const elAbout = document.querySelector('.about-section')
+  elGallery.scrollIntoView({ behavior: "smooth" })
+}
+function onShowAbout() {
+  const elAbout = document.querySelector(".about-section")
 
-        elAbout.scrollIntoView({ behavior: "smooth",})
-       }
+  elAbout.scrollIntoView({ behavior: "smooth" })
+}
+function onHireMe() {
+  window.location.replace("https://www.linkedin.com/in/soli-kolet-58b5a6191/")
+}
